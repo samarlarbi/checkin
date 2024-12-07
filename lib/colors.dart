5466d48313b6436ba0865c1e857260a0eec6dbdf
samar <samarlarbi2004@gmail.com>
2024-12-08 00:37:10 +0100

    add app bar

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 const Primary = Color.fromARGB(255, 44, 108, 110);
 const Secondary = Color.fromARGB(255, 113, 156, 157);
 const light = Color.fromARGB(255, 170, 171, 171);
-const Background = Color.fromARGB(255, 217, 217, 217);
+const Background = Color.fromARGB(255, 235, 235, 235);
